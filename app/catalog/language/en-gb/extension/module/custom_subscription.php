92abdc7b180f674ed0a9054e520271692624f5d7
Anton Podlesnyy <antlogist@yandex.ru>
2022-05-16 10:23:59 +0300

    Subscription lexicon files created

---
+++ app/catalog/language/en-gb/extension/module/custom_subscription.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Subscription';
+
+// Text
+$_['text_email']      = 'Email:';
+$_['text_placeholder']      = 'Enter your Email';
+
+// Button
+$_['btn_subscription']      = 'Subscribe';
---
+++ app/catalog/language/ru-ru/extension/module/custom_subscription.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Подписка';
+
+// Text
+$_['text_email']      = 'Email:';
+$_['text_placeholder']      = 'Введите ваш Email';
+
+// Button
+$_['btn_subscription']      = 'Подписаться';
